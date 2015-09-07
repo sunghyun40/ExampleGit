34f53716cc10e5f4a1692565c9a59f0bf61f3d92
ExampleGitCommit <tn3728@naver.com>
2015-09-07 10:45:04 +0900

    변수정의

--- app/src/main/java/dd/examplegit/MainActivity.java
+++ app/src/main/java/dd/examplegit/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
